0e3a8c6585d60b3fc3ab81fe2d5b769375b8dc59
Finlay Norton-Lindsay (he/him) <p3n.duke@gmail.com>
2025-12-02 10:18:05 -0800

    Bash Scripts

---
+++ gbm_download.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+while read line
+do
+   for k in 0 1 2 3 4 5 6 7 8 9;
+   do
+   for j in 0 1 2 3 4 5 6 7 8 9;
+   do
+   for w in 0 1 2 3 4 5 6 7 8 9;
+   do
+   SUB=0
+   if [$w==0]; then
+      if [$j==0]; then
+         SUB="${k}"
+      fi
+      SUB="${k}${j}"
+   fi
+   SUB="${k}${j}${w}"
+
+   IFS="%" read -r TRIGGERa <<< "$line"
+   DATE=$(cut -b 1-2 <<< "${TRIGGERa}")
+   TRIGGERb=$(sed 's/\r//g' <<< "${TRIGGERa}")
+   TRIGGER="${TRIGGERb}${SUB}"
+   echo "Downloading Trigger: ${TRIGGER}"
+   echo "Year 20${DATE}"
+   for i in 1 2 3 4 5 6 7 8 9;
+   do
+   wget https://heasarc.gsfc.nasa.gov/FTP/fermi/data/gbm/bursts/20${DATE}/bn${TRIGGER}/current/glg_tte_n${i}_bn${TRIGGER}_v00.fit
+   wget https://heasarc.gsfc.nasa.gov/FTP/fermi/data/gbm/bursts/20${DATE}/bn${TRIGGER}/current/glg_tte_n${i}_bn${TRIGGER}_v01.fit
+   mv glg_tte_n${i}_bn${TRIGGER}_v01.fit GBM_files/glg_tte_n${i}_bn${TRIGGER}_v00.fit
+   mv glg_tte_n${i}_bn${TRIGGER}_v01.fit GBM_files/glg_tte_n${i}_bn${TRIGGER}_v01.fit; 
+   done
+   done
+   done
+   done
+done < grb_triggers.csv
