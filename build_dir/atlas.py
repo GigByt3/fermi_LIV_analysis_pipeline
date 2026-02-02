4494d1749ea83cd732596afa62332a4599bb62a0
Finlay Norton-Lindsay (he/him) <p3n.duke@gmail.com>
2026-02-02 12:10:19 -0800

    Revert "index error correction"
    This reverts commit ba69c9eeb8d13d37f51520b65814e76415ff416a.

--- build_dir/atlas.py
+++ build_dir/atlas.py
@@ -77,8 +77,8 @@ class GBM_scraper:
             sliced_counts = working_save["time_array"]
             print("Modifying array " + str(trig_number) + " of length: " + str(len(sliced_counts)))
 
-        for i in range(0, len(sliced_counts_prime)):
-            for j in range(0, len(sliced_counts_prime[i])):
+        for i in range(0, len(sliced_counts_prime)-1):
+            for j in range(0, len(sliced_counts_prime[i])-1):
                 sliced_counts[j] = sliced_counts[j] + sliced_counts_prime[i][j][0]
 
         save_array[trig_number]["time_values"] = time_counts
