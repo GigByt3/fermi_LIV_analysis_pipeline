75c6443e15d271cb47e619c39fc638eff4daa3ae
Finlay Norton-Lindsay (he/him) <p3n.duke@gmail.com>
2026-02-04 11:28:24 -0800

    Update sextant.py

--- build_dir/sextant.py
+++ build_dir/sextant.py
@@ -114,7 +114,7 @@ for name in save_array:
     print(name, flush=True)
     result = atlas.redback_scraper.pull(name, save_array, db)
     result[name]["time_array"] = result[name]["time_array"].tolist()
-    result[name]["time_value"] = result[name]["time_value"].tolist()
+    result[name]["time_values"] = result[name]["time_values"].tolist()
     result[name]["half_max_t"] = result[name]["half_max_t"][0]
     save_array = result
 
